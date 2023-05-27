3f2c7a5b0dea76827cab1807826c7a46fff59e18
jynt1401 <100084399+jynt1401@users.noreply.github.com>
2023-05-27 23:21:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://cryptofolio-full-stack-1.vercel.app/",
     credentials: true,
   })
 );
