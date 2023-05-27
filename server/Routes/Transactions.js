05fc44540e4c10e623550c0af599f452b330c8e9
jynt1401 <100084399+jynt1401@users.noreply.github.com>
2023-05-27 23:14:08 +0530

    Update Transactions.js

--- server/Routes/Transactions.js
+++ server/Routes/Transactions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {UserTransactions} = require("../Controllers/userTransactions");
+const {UserTransactions} = require("../Controllers/UserTransactions");
 const {UserSellTransactions} = require("../Controllers/UserSellTransactions");
 
 router.post("/transactions", UserTransactions);
